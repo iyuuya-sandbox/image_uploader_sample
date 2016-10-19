28e0230da13e0de26395bd404db73a1d8e7cd3b5
iyuuya <vim.ito@1pac.jp>
2016-10-19 17:52:35 +0900

    wip

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,10 @@
 class Image < ApplicationRecord
-  belongs_to :fileable, polymorphic: true
+  belongs_to :fileable, polymorphic: true, required: false
+
+  validates :file, presence: true
+  validates :name, presence: true
+  validates :content_type, presence: true
+  validates :digest, presence: true
+
+  mount_uploader :file, ImageUploader
 end
