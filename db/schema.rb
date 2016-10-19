f07c2023c269a6ad09e9c308aa76ba9803b5c37c
iyuuya <vim.ito@1pac.jp>
2016-10-19 17:59:36 +0900

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019083911) do
+ActiveRecord::Schema.define(version: 20161019085844) do
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "fileable_type"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20161019083911) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.index ["fileable_type", "fileable_id"], name: "index_images_on_fileable_type_and_fileable_id", using: :btree
+  end
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
