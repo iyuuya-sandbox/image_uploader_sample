1491330ef313175f071216db66e457dacc47205a
iyuuya <vim.ito@1pac.jp>
2016-10-19 18:01:52 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'top#index'
+
   resources :posts
   resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
